a6888f28f54364af2730b59e3bc93b51157e4e17
Alexander Florez Rodriguez <alexflorezr@gmail.com>
2015-08-19 11:21:56 +0200

    Legend added

--- climate_change.R
+++ climate_change.R
@@ -37,32 +37,36 @@ for (s in seq_along(Single_sp)){
 ### setwd("/Users/afr/Desktop/kk_temp/")
 ### write.table(x=temp_points, file=paste(tolower(Single_sp[s]), "_vel_bin.txt", sep=""), sep="\t",row.names=F)
 ### species_sp <- paste(strsplit(strsplit(tolower(Single_sp[s]), split = "_")[[1]][1], split = "")[[1]][1], strsplit(strsplit(tolower(Single_sp[s]), split = "_")[[1]][2], split = "")[[1]][1], sep="")
-### bp <- boxplot(points_plot$Velocity ~ as.numeric(points_plot$Time_bin), main=Single_sp[s], border="#EE760095", boxwex=.7, col="#EE760095", at=tick/1000, las=2, yaxt="n", frame=F, xaxt="n",  cex=0.5, xlim=c(0,50))
-### tick_labels <- c(0, rep(NA, 4), 5000, rep(NA, 4), 10000, rep(NA, 4), 15000, rep(NA, 4), 20000, rep(NA, 4), 25000, rep(NA, 4), 30000, rep(NA, 4), 35000, rep(NA, 4), 40000, rep(NA, 4), 45000, rep(NA, 4), 50000)
-### axis(side=1, at=seq(0, 50, by=1), labels=tick_labels, las=2, cex=0.5)
-### axis(side=4, at=seq(0, 1, by=0.2), cex=0.5)
-### lines(as.numeric(bp$names)/1000, bp$stats[3,], col="#EE7600", lwd=2)
+bp <- boxplot(points_plot$Velocity ~ as.numeric(points_plot$Time_bin), main=Single_sp[s], border="#EE760095", boxwex=.7, col="#EE760095", at=tick/1000, las=2, yaxt="n", frame=F, xaxt="n",  cex=0.5, xlim=c(0,50))
+tick_labels <- c(0, rep(NA, 4), 5000, rep(NA, 4), 10000, rep(NA, 4), 15000, rep(NA, 4), 20000, rep(NA, 4), 25000, rep(NA, 4), 30000, rep(NA, 4), 35000, rep(NA, 4), 40000, rep(NA, 4), 45000, rep(NA, 4), 50000)
+ axis(side=1, at=seq(0, 50, by=1), labels=tick_labels, las=2, cex=0.5)
+ axis(side=4, at=seq(0, 1, by=0.2), cex=0.5)
+ lines(as.numeric(bp$names)/1000, bp$stats[3,], col="#EE7600", lwd=2)
   image_climate[s,as.vector(na.omit(match(bp$names, colnames(image_climate))))] <- bp$stats[3,]
   image_climate_rank[s,as.vector(na.omit(match(bp$names, colnames(image_climate))))] <- rank(bp$stats[3,])
 }
 
-
+library(SDMTools)
 colores <- colorRampPalette(c("#C6E2FF", "#63B8FF", "#1E90FF", "#7B68EE", "#D15FEE", "#FF69B4", "#EE3A8C", "#FF0000"))(38)
-par(mar=c(4,10.5,4,4))
-#image(t(image_climate), col=paste(colores, 90, sep=""), axes=F)
-image(t(image_climate_rank), axes=F, col=paste(colores, 99, sep=""))
+par(mar=c(5,12,4,4))
+image(log(t(image_climate)), col=paste(colores, 90, sep=""), axes=F)
+image(t(image_climate_rank), axes=F, col=paste(colores, 99, sep=""), xlim=c(0,1.3))
 axis(1, at=(seq(0,1,by=1/36)), labels=colnames(image_climate_rank), las=2)
 axis(2, at=(seq(0,1,by=1/20)), labels=rownames(image_climate_rank), las=2)
 #axis(4, at=(seq(0,1,by=1/20)), labels=rowsum(image_climate_rank, ), las=2)
 for (x in 1:ncol(image_climate_rank)){
   for (y in 1:nrow(image_climate_rank)){
     extemes<- c(max(t(image_climate_rank)[,y], na.rm=T), min(t(image_climate_rank)[,y], na.rm=T))
-    text((x-1)/36, (y-1)/20, t(image_climate_rank)[x,y], cex=ifelse(is.na(sum(match(t(image_climate_rank)[x,y],extemes))),0.5, 1.3))
+    text((x-1)/36, (y-1)/20, t(image_climate_rank)[x,y], cex=ifelse(is.na(sum(match(t(image_climate_rank)[x,y],extemes))),0.3, 0.9))
     #print(sum(match(t(image_climate_rank)[x,y],extemes)))
   }
 }
+legend.gradient(cbind(c(1.05,1.2,1.2,1.05), c(0.95,0.95,0.7,0.7)), cols=paste(colores, 99, sep=""), limits=c("Slowest", "Fastest"), title="Ranking")
 
-
+min(as.vector(na.omit(image_climate)))
+which(image_climate == min(image_climate), arr.ind = TRUE)
+min(image_climate,na.rm=T)
+max(image_climate,na.rm=T)
 ##############################
 ### Alex: Correlate to BSP ###
 ##############################
